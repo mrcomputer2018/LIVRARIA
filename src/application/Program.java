ab6a153a5d433f395a9e07b6f4630a6c24c19f3f
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2022-04-20 10:08:38 -0300

    capitulo 4.2

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,14 +3,30 @@
 */
 package application;
 
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Scanner inputChar = new Scanner(System.in);
 
-        CadastroDeLivros cadastroDeLivros = new CadastroDeLivros();
-        System.out.println("Testando sistema");
-        System.out.println("***** MENU *****");
-        System.out.println("( 1 )- Cadastrar Livros");
-        System.out.println();
-        cadastroDeLivros.cadastrar();
+        try {
+            CadastroDeLivros cadastroDeLivros = new CadastroDeLivros();
+            System.out.println("====== SISTEMA LIVRARIA =====");
+            System.out.println("***** MENU *****");
+            System.out.println("( 1 ) - Cadastrar Livros");
+            System.out.println("( 2 ) - Aplicar desconto");
+            System.out.println("Digite a opção desejada :");
+            int menu = input.nextInt();
+
+            switch (menu) {
+                case 1:
+                    System.out.println();
+                    cadastroDeLivros.cadastrar();
+            }
+        }
+        catch() {
+
+        }
     }
  }
