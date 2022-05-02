817baaefde71c9e053b1df8d542f33e469256532
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2022-05-02 10:35:34 -0300

    versao 9.1 - array em CarrinhoDeCompras.java

--- src/application/CarrinhoDeCompras.java
+++ src/application/CarrinhoDeCompras.java
@@ -4,6 +4,8 @@ import entities.Produto;
 
 public class CarrinhoDeCompras {
     private double total = 0;
+    private Produto[] produtos = new Produto[10];
+    private int contador = 0;
 
     // Getters
     public double getTotal(){
@@ -14,7 +16,10 @@ public class CarrinhoDeCompras {
     public void adiciona(Produto produto) {
         System.out.println("Adicionando: " + produto);
         // Aplica desconto a cada venda
-       // produto.aplicaDescontoDe(0.05);
+        // produto.aplicaDescontoDe(0.05);
+        this.produtos[contador] = produto;
+        contador ++;
+
         this.total = this.total + produto.getValor();
     }
 }
